5fd5f7f62fd72aead80e60565336864c2d4fe189
José Ithalo <jose_ithalo@hotmail.com>
2023-07-08 22:06:04 -0300

    Implementações na classe Vendas

--- src/Vendas.ts
+++ src/Vendas.ts
@@ -1,10 +1,35 @@
 import { Carrinho } from "./Carrinho";
+import { formartarNum } from "./formatar";
 
 export class Vendas extends Carrinho {
 
     constructor() {
         super();
+    }
 
+    finalizarPedido(opcao: string): string {
+
+        let totalVenda: number = 0;
+
+        for (let item of this.listaCarrinho) {
+            totalVenda += (item.valor * item.quantidade);
+        }
+
+        if (opcao.toLocaleLowerCase() === 'crédito') {
+            if (totalVenda > 10000) {
+                totalVenda -= totalVenda * 5 / 100;
+                return `Pedido finalizado. Total: R$ ${formartarNum(totalVenda)}`;
+            }
+            return `Pedido finalizado. Total: R$ ${formartarNum(totalVenda)}`;
+        }
+
+        if (opcao.toLocaleLowerCase() === 'débito') {
+            totalVenda -= totalVenda * 7 / 100;
+
+            return `Pedido finalizado. Total: R$ ${formartarNum(totalVenda)}`;
+        }
+
+        return 'Opção inválida de pagamento.\nEscolha crédito ou débito.'
     }
 
 }
