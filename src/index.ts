863d3662f967c417196585cfa983819c58d9afd2
José Ithalo <jose_ithalo@hotmail.com>
2023-07-09 21:28:28 -0300

    Implementação de código para execução

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
-import { Carrinho } from "./Carrinho";
+import { Vendas } from "./Vendas";
 
-const carrinho1 = new Carrinho({
+const venda1 = new Vendas();
+
+venda1.adicionar({
+    id: 1,
     descricao: 'Camisa',
     categoria: 'Roupa',
     valor: 5000,
     quantidade: 1
 });
 
-console.log(carrinho1.imprimirResumo());
+venda1.adicionar({
+    id: 2,
+    descricao: 'Mouse',
+    categoria: 'Eletrônico',
+    valor: 7500,
+    quantidade: 2
+});
 
+venda1.adicionar({
+    id: 3,
+    descricao: 'Cama',
+    categoria: 'Móveis',
+    valor: 20000,
+    quantidade: 1
+});
+
+console.log(venda1.remover(3));
+
+console.log(venda1.alterarQuantidade(1, 2));
+
+console.log(venda1.imprimirResumo());
+
+// console.log(venda1.finalizarPedido('crédito'));
+console.log(venda1.finalizarPedido('débito'));
